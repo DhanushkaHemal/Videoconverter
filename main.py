6e38ddc8b29b7e3a0e1100258c306ccd9fea0096
dhemal2001 <dhemal2001@gmail.com>
2024-05-31 22:11:59 +0530

    init

--- main.py
+++ main.py
@@ -6,40 +6,34 @@ import re
 import sys
 import time
 import os
-import pipes
+import shlex
 
 def video_to_audio(fileName):
-	try:
-		file, file_extension = os.path.splitext(fileName)
-		file = pipes.quote(file)
-		video_to_wav = 'ffmpeg -i ' + file + file_extension + ' ' + file + '.wav'
-		final_audio = 'lame '+ file + '.wav' + ' ' + file + '.mp3'
-		os.system(video_to_wav)
-		os.system(final_audio)
-		#file=pipes.quote(file)
-		#os.remove(file + '.wav')
-		print("sucessfully converted ", fileName, " into audio!")
-	except OSError as err:
-		print(err.reason)
-		exit(1)
+    try:
+        file, file_extension = os.path.splitext(fileName)
+        safe_file = shlex.quote(file)  # Use shlex.quote for security
+        safe_fileName = shlex.quote(fileName)
+        video_to_mp3 = f'ffmpeg -i {safe_fileName} -q:a 0 -map a {safe_file}.mp3'
+        os.system(video_to_mp3)
+        print("Successfully converted", fileName, "into audio!")
+    except OSError as err:
+        print("OS error:", err)
+        exit(1)
 
 def main():
-	if len(sys.argv) <1 or len(sys.argv) > 2:
-		print('command usage: python3 video_to_audio.py FileName')
-		exit(1)
-	else:
-		filePath = sys.argv[1]
-		# check if the specified file exists or not
-		try:
-			if os.path.exists(filePath):
-				print("file found!")
-		except OSError as err:
-			print(err.reason)
-			exit(1)
-		# convert video to audio
-		video_to_audio(filePath)
-		time.sleep(1)
-		
-# install ffmpeg and/or lame if you get an error saying that the program is currently not installed 
+    if len(sys.argv) != 2:
+        print('command usage: python3 video_to_audio.py FileName')
+        exit(1)
+    else:
+        filePath = sys.argv[1]
+        # Check if the specified file exists
+        if os.path.exists(filePath):
+            print("file found!")
+            video_to_audio(filePath)
+        else:
+            print("File not found.")
+            exit(1)
+
+# Install ffmpeg if you get an error saying that the program is currently not installed
 if __name__ == '__main__':
-	main()
+    main()
